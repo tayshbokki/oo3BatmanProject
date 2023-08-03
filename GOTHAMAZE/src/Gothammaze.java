983eccf4b6761789ea220d7ddfa452676bcadc1b
Banananalden <137026876+Banananalden@users.noreply.github.com>
2023-08-03 15:26:35 +0800

    Update Gothammaze.java

--- GOTHAMAZE/src/Gothammaze.java
+++ GOTHAMAZE/src/Gothammaze.java
@@ -8,18 +8,30 @@ public class Gothammaze {
 
     public Gothammaze(int size) {
         this.size = size;
-        this.mazeLayouts = new int[3][size][size];
+        this.mazeLayouts = new int[8][size][size]; // Increase the size to 5 to accommodate layouts 1 to 5
         generateLayout1();
         generateLayout2();
         generateLayout3();
-        this.currentLayout = 0; // Start with the first layout
+        generateLayout4();
+        generateLayout5();
+        generateLayout6();
+        this.currentLayout = 0; // Start with layout 1
     }
 
     public int[][] generateMaze() {
         int[][] maze = mazeLayouts[currentLayout];
-        currentLayout = (currentLayout + 1) % 3; // Cycle to the next layout
+        currentLayout = (currentLayout + 1) % mazeLayouts.length; // Cycle to the next layout
         return maze;
     }
+
+    public int getCurrentLayout() {
+        return currentLayout + 1; // Return the current layout number (1-based index)
+    }
+
+    public int getTotalLayouts() {
+        return mazeLayouts.length; // Return the total number of layouts
+    }
+
     //1 is free 0 is wall
     private void generateLayout1() {
         // Define the maze layout 1
@@ -30,7 +42,7 @@ public class Gothammaze {
                 {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
                 {0, 1, 1, 1, 1, 1, 0, 1, 1, 0},
                 {0, 1, 0, 0, 0, 1, 0, 1, 0, 0},
-                {0, 1, 0, 0, 0, 2, 0, 1, 0, 0},
+                {0, 1, 0, 0, 0, 1, 0, 1, 0, 0},
                 {0, 1, 1, 0, 1, 0, 0, 1, 0, 0},
                 {0, 1, 0, 0, 1, 1, 1, 1, 1, 1},
                 {0, 1, 0, 0, 0, 0, 0, 0, 0, 0}
@@ -45,8 +57,8 @@ public class Gothammaze {
                 {1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
                 {1, 0, 1, 0, 1, 0, 0, 0, 0, 1},
                 {1, 1, 1, 0, 1, 0, 0, 1, 1, 1},
-                {0, 0, 0, 0, 1, 0, 0, 1, 0, 0},
-                {0, 0, 0, 1, 1, 1, 1, 1, 1, 1},
+                {0, 2, 2, 0, 1, 0, 0, 1, 0, 0},
+                {0, 2, 2, 1, 1, 1, 1, 1, 1, 1},
                 {0, 0, 0, 1, 0, 0, 0, 0, 0, 1},
                 {0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
                 {0, 0, 0, 1, 0, 0, 0, 1, 1, 1},
@@ -70,5 +82,54 @@ public class Gothammaze {
                 {0, 0, 1, 1, 1, 0, 1, 0, 0, 0}
         };
         mazeLayouts[2] = layout3;
+    }
+
+    private void generateLayout4() {
+        // Define the maze layout 4 with a car that blocks a path every two turns
+        int[][] layout4A = {
+                {1, 1, 1, 1, 1, 1, 2, 2, 2, 2},
+                {0, 0, 1, 0, 0, 1, 2, 2, 2, 2},
+                {0, 0, 1, 0, 0, 1, 1, 1, 2, 2},
+                {0, 0, 1, 1, 1, 1, 0, 1, 2, 2},
+                {0, 0, 1, 0, 0, 0, 0, 1, 2, 2},
+                {0, 0, 1, 0, 0, 0, 0, 1, 2, 2},
+                {0, 0, 1, 0, 0, 0, 0, 3, 2, 2},
+                {0, 0, 1, 1, 1, 1, 0, 1, 1, 1},
+                {0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+                {0, 0, 0, 0, 0, 1, 1, 1, 1, 1}
+        };
+        mazeLayouts[3] = layout4A;
+    }
+
+    private void generateLayout5() {
+        int[][] layout4B = {
+                {1, 1, 1, 1, 1, 1, 2, 2, 2, 2},
+                {0, 0, 1, 0, 0, 1, 2, 2, 2, 2},
+                {0, 0, 1, 0, 0, 1, 1, 1, 2, 2},
+                {0, 0, 1, 1, 1, 1, 0, 1, 2, 2},
+                {0, 0, 1, 0, 0, 0, 0, 1, 2, 2},
+                {0, 0, 3, 0, 0, 0, 0, 1, 2, 2},
+                {0, 0, 1, 0, 0, 0, 0, 1, 2, 2},
+                {0, 0, 1, 1, 1, 1, 0, 1, 1, 1},
+                {0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+                {0, 0, 0, 0, 0, 1, 1, 1, 1, 1}
+        };
+        mazeLayouts[4] = layout4B;
+    }
+
+    private void generateLayout6() {
+        int[][] layout6 = {
+                {1, 1, 1, 0, 1, 1, 1, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 2, 1, 0, 0, 1},
+                {0, 0, 0, 0, 1, 2, 1, 0, 1, 1},
+                {0, 1, 1, 1, 1, 2, 1, 0, 0, 0},
+                {0, 1, 0, 0, 0, 1, 1, 1, 0, 0},
+                {0, 1, 1, 1, 1, 2, 0, 1, 0, 0},
+                {0, 1, 1, 0, 1, 2, 2, 1, 1, 0},
+                {0, 1, 0, 0, 1, 1, 2, 0, 1, 1},
+                {0, 0, 0, 1, 1, 1, 1, 1, 1, 0}
+        };
+        mazeLayouts[5] = layout6;
     }
 }
